b9ef842cb5743745c55911e238f0b406a9644f04
Cernan Bernardo <cernanb@gmail.com>
2018-07-29 23:12:51 -0700

    Add logout to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,51 @@
-import React from 'react'
+import React, { Fragment, Component } from 'react'
 import ParcelLogo from '../img/logo.svg'
 import { Link } from '@reach/router'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
-const Header = props => {
-  return (
-    <header>
-      <nav className="navbar is-light" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Link className="navbar-item" to="/">
-            <img width="60" src={ParcelLogo} alt="" />
-          </Link>
-        </div>
-        <div className="navbar-end">
-          <Link className="navbar-item" to="/signup">
-            Signup
-          </Link>
-          <Link className="navbar-item" to="/signin">
-            Sign In
-          </Link>
-          <Link className="navbar-item" to="/clients">
-            Clients
-          </Link>
-        </div>
-      </nav>
-    </header>
-  )
+class Header extends Component {
+  logout = e => {
+    e.preventDefault()
+    this.props.mutate({
+      refetchQueries: [{ query }],
+    })
+  }
+  render() {
+    const { data } = this.props
+    return (
+      <header>
+        <nav className="navbar is-light" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <Link className="navbar-item" to="/">
+              <img width="60" src={ParcelLogo} alt="" />
+            </Link>
+          </div>
+          <div className="navbar-end">
+            {data.currentUser ? (
+              <Fragment>
+                <Link className="navbar-item" to="/clients">
+                  Clients
+                </Link>
+                <Link className="navbar-item" to="" onClick={this.logout}>
+                  Log out
+                </Link>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <Link className="navbar-item" to="/signup">
+                  Signup
+                </Link>
+                <Link className="navbar-item" to="/signin">
+                  Sign In
+                </Link>
+              </Fragment>
+            )}
+          </div>
+        </nav>
+      </header>
+    )
+  }
 }
 
 const query = gql`
@@ -39,4 +58,12 @@ const query = gql`
   }
 `
 
-export default graphql(query)(Header)
+const mutation = gql`
+  mutation {
+    logout {
+      firstName
+    }
+  }
+`
+
+export default graphql(mutation)(graphql(query, mutation)(Header))

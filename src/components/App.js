3a47dfca70665d443885e85020d4a55010359460
Cernan Bernardo <cernanb@gmail.com>
2018-09-08 00:34:21 -0700

    Start ClientShow component

--- src/components/App.js
+++ src/components/App.js
@@ -3,28 +3,30 @@ import Header from './Header'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Clients from './Clients'
+import ClientShow from './ClientShow'
 import Signup from './Signup'
 import Signin from './Signin'
 import Home from './Home'
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Header />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/clients" component={Clients} />
-              <Route path="/signin" component={Signin} />
-              <Route path="/signup" component={Signup} />
-            </Switch>
-          </div>
-        </div>
-      </Router>
-    )
-  }
+   render() {
+      return (
+         <Router>
+            <div>
+               <Header />
+               <div className="container">
+                  <Switch>
+                     <Route exact path="/" component={Home} />
+                     <Route path="/clients/:id" component={ClientShow} />
+                     <Route exact path="/clients" component={Clients} />
+                     <Route path="/signin" component={Signin} />
+                     <Route path="/signup" component={Signup} />
+                  </Switch>
+               </div>
+            </div>
+         </Router>
+      )
+   }
 }
 
 export default App
---
+++ src/components/ClientShow.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class ClientShow extends Component {
+   render() {
+      return <div>Client Show</div>
+   }
+}

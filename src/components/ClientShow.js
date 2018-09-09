9258ea4923d9d1ab8ea45bc845952bd64ea35736
Cernan Bernardo <cernanb@gmail.com>
2018-09-09 00:32:46 -0700

    Start ClientShow component

--- src/components/ClientShow.js
+++ src/components/ClientShow.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import ReactLoading from 'react-loading'
 
 export default class ClientShow extends Component {
-   render() {
-      return <div>Client Show</div>
-   }
+  render() {
+    return (
+      <Query
+        query={gql`{
+          client(id: "${this.props.match.params.id}") {
+            id
+            firstName
+            lastName
+            opportunities {
+              id
+              product
+            }
+          }
+        }
+`}
+      >
+        {({ data, loading }) => {
+          return loading ? (
+            <ReactLoading
+              className="is-horizontal-center"
+              type={'spinningBubbles'}
+              color={'#87adec'}
+              height={'20%'}
+              width={'20%'}
+            />
+          ) : (
+            <h1>{data.client.firstName}</h1>
+          )
+        }}
+      </Query>
+    )
+  }
 }

1a29208511c15f9cdc864d28e6bd191776846e1c
Cernan Bernardo <cernanb@gmail.com>
2018-09-09 00:32:02 -0700

    Add react loading spinner

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -2,43 +2,50 @@ import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
 import ClientCard from './ClientCard'
+import ReactLoading from 'react-loading'
 
 class Clients extends Component {
-   renderClients = clients => {
-      return clients.map(client => <ClientCard history={this.props.history} key={client.id} client={client} />)
-   }
-   render() {
-      return (
-         <Query query={clientsQuery}>
-            {({ loading, data }) => {
-               return loading ? (
-                  <h1>Loading</h1>
-               ) : (
-                  <div>
-                     <h1>Clients</h1>
-                     <div className="columns is-multiline">{this.renderClients(data.clients)}</div>
-                  </div>
-               )
-            }}
-         </Query>
-      )
-   }
+  renderClients = clients => {
+    return clients.map(client => <ClientCard history={this.props.history} key={client.id} client={client} />)
+  }
+  render() {
+    return (
+      <Query query={clientsQuery}>
+        {({ loading, data }) => {
+          return loading ? (
+            <ReactLoading
+              className="is-horizontal-center"
+              type={'spinningBubbles'}
+              color={'#87adec'}
+              height={'20%'}
+              width={'20%'}
+            />
+          ) : (
+            <div>
+              <h1>Clients</h1>
+              <div className="columns is-multiline">{this.renderClients(data.clients)}</div>
+            </div>
+          )
+        }}
+      </Query>
+    )
+  }
 }
 
 const clientsQuery = gql`
-   {
-      currentUser {
-         id
-         email
-      }
+  {
+    currentUser {
+      id
+      email
+    }
 
-      clients {
-         id
-         firstName
-         lastName
-         email
-      }
-   }
+    clients {
+      id
+      firstName
+      lastName
+      email
+    }
+  }
 `
 
 export default Clients

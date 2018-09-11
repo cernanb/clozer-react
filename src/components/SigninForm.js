27a18e3f29321facbe7c3cc96d2e45abeb7bd4d0
Cernan Bernardo <cernanb@gmail.com>
2018-09-10 21:21:52 -0700

    Create SigninForm

---
+++ src/components/SigninForm.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+export default class SigninForm extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  render() {
+    const { email, password } = this.state
+    return (
+      <Mutation mutation={LOGIN} variables={{ email, password }}>
+        {login => (
+          <form
+            onSubmit={e => {
+              e.preventDefault()
+              login()
+                .then(() => {
+                  this.setState({ email: '', password: '' })
+                  this.props.history.push('/clients')
+                })
+                .catch(e => console.log(e))
+            }}
+          >
+            <div className="field">
+              <label className="label is-left">Email</label>
+              <div className="control">
+                <input onChange={this.onChange} name="email" className="input" type="text" />
+              </div>
+              <label className="label is-left">Password</label>
+              <div className="control">
+                <input onChange={this.onChange} name="password" className="input" type="password" />
+              </div>
+              <input value="Sign in" type="submit" />
+            </div>
+          </form>
+        )}
+      </Mutation>
+    )
+  }
+}
+
+const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      email
+    }
+  }
+`
